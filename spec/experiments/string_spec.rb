a8c0d9e4fa89b8fd0517cbe6eda546579d2af9b3
seiyaKM <seiya.kumada.0107@gmail.com>
2023-03-05 21:39:00 +0000

    コントローラを追加

--- spec/experiments/string_spec.rb
+++ spec/experiments/string_spec.rb
@@ -1,4 +1,11 @@
-class ClassName
-  def method_name
+require 'spec_helper'
+
+describe String do
+  describe '#<<' do
+    example '文字列の追加' do
+      s = 'ABC'
+      s << 'D'
+      expect(s.size).to eq(4)
+    end
   end
 end
