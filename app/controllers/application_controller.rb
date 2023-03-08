c5612b7f613f3f57406bc1bf7f4e2cc88fcf0644
seiyaKM <seiya.kumada.0107@gmail.com>
2023-03-08 13:48:44 +0000

    staff page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  layout :set_layout
+
+  private def set_layout
+    params[:controller].match(%r{\A(staff|admin|customer)/}) ? Regexp.last_match[1] : 'customer'
+  end
 end
